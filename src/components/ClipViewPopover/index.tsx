1c7bf6331b661856ef1c32538faccecc7960b496
uxstorysoyeon <sysy0313@uxstory.co.kr>
2024-12-30 18:26:38 +0900

    [퍼블리싱]_241230_클립선택 스타일 수정

--- src/components/ClipViewPopover/index.tsx
+++ src/components/ClipViewPopover/index.tsx
@@ -252,6 +252,15 @@ const ClipRangeWrapper = styled.div`
     height: 55%;
     position: relative;
     top: 20%;
+    .hogak-clip-slider {
+        //241230 추가
+        /* 전체화면일 때 */
+        height: calc(100% + 0.6em);
+        top: -0.3em;
+        /* 전체화면 아닐 때 */
+        height: calc(100% + 1.4em);
+        top: -0.7em;
+    }
 `
 // 클립 슬라이더 선택 바
 const ClipThumb = styled.div`
