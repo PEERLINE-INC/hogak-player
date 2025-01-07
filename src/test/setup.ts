430ec3022c7b5a44e5171a90006d46d8120b5f4c
gil yunseo <yskil@peerline.net>
2025-01-07 12:58:31 +0900

    add mock

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -10,6 +10,20 @@ class ResizeObserver {
 
 global.ResizeObserver = ResizeObserver;
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
 // hooks are reset before each suite
 afterEach(() => {
   cleanup()
